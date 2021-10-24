ab02552c6e9f07de71eff40464b8e7dba5cccd61
Brandon! <brandoge91@gmail.com>
2021-10-24 11:17:54 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+const request = require('request-promise');
+const fs = require("fs");
+
+module.exports = function(apiKey) {
+  return new Client(apiKey)
+}
+
+
+
+function Client(apiKey) {
+  this.apiKey = apiKey;
+}
+
+Client.prototype.predict = function(options) {
+  modelType = options.modelType
+  modelId = options.modelId
+  filePath = options.filePath || ''
+  fileURL = options.fileURL
+  fileName = filePath ? filePath.split('/')[filePath.split('/').length - 1] : ''
+/*
+  let urlNanoNets = `https://app.nanonets.com/api/v2/${modelType}/Model/${modelId}/LabelFile/`
+  var urlOptions = {
+    'method': 'POST',
+    'url': urlNanoNets,
+    'headers': {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization' : "Basic " + Buffer.from(this.apiKey + ":").toString("base64")
+    },
+    'formData' : {
+      'modelId': modelId,
+    }
+  }
+
+  if(fileURL){
+    urlOptions.formData = {
+      'urls' : fileURL,
+      'modelId': modelId
+    }
+  }
+  if(filePath) {
+      urlOptions.formData = {
+      'modelId': modelId,
+        'file': {
+        'value': fs.createReadStream(filePath),
+        'options': { 
+          'filename': fileName,
+          'contentType': null
+        }
+      }
+    }
+  }
+
+  */
+  const form_data = {'modelId' : modelId, 'file' : fs.createReadStream(filePath)}
+  options = {
+      url : 'https://app.nanonets.com/api/v2/ImageCategorization/LabelFile/',
+      formData: form_data,
+      headers: {
+          'Authorization' : 'Basic ' + Buffer.from(this.apiKey + ':').toString('base64')
+      }
+  }
+  request.post(options, function(err, httpResponse, body) {
+  
+    console.log(body)
+  });
+  
+  return request(options)
+  
+}
